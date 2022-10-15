628dc340a4bccbf656cfd6a65e4521851fd38bc5
Slava <110696118+sl7one@users.noreply.github.com>
2022-10-15 19:23:30 +0300

    update task 01

---
+++ js/task-01.js
@@ -0,0 +1,22 @@
+const categoriesEl = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesEl.length}`);
+
+console.log('');
+
+// for (let el of categoriesEl) {
+//   let categoryName = el.querySelector('h2').textContent;
+//   let countCategoryItems = el.querySelector('ul').querySelectorAll('li').length;
+//   console.log(`Category: ${categoryName}`);
+
+//   console.log(`Elements: ${countCategoryItems}`);
+//   console.log('');
+// }
+
+categoriesEl.forEach(el => {
+  let categoryName = el.querySelector('h2').textContent;
+  let countCategoryItems = el.querySelector('ul').querySelectorAll('li').length;
+  console.log(`Category: ${categoryName}`);
+
+  console.log(`Elements: ${countCategoryItems}`);
+  console.log('');
+});
