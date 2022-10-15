811e289e3ca5bc2772b95773207b10f6b541affa
Slava <110696118+sl7one@users.noreply.github.com>
2022-10-15 23:11:57 +0300

    update task 02

--- js/task-01.js
+++ js/task-01.js
@@ -15,6 +15,7 @@ console.log('');
 categoriesEl.forEach(el => {
   let categoryName = el.querySelector('h2').textContent;
   let countCategoryItems = el.querySelector('ul').querySelectorAll('li').length;
+  // let countCategoryItems = el.querySelector('ul', 'li').length;
   console.log(`Category: ${categoryName}`);
 
   console.log(`Elements: ${countCategoryItems}`);
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+// const ingredients2 = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Лук', 'Специи', 'Буряк', 'Лапша'];
+
+const makeElList = el => {
+  const newElArray = el.map(el => {
+    const liEl = document.createElement('li');
+    liEl.classList.add('item');
+    liEl.textContent = el;
+    return liEl;
+  });
+
+  const newElList = document.createElement('ul');
+
+  newElList.append(...newElArray);
+  return newElList;
+};
+
+document.body.append(makeElList(ingredients));
+// document.body.append(makeElList(ingredients2));
