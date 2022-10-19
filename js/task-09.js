3c60ea59c0218c11c4887dbdf62eab6d658e4e73
Slava <110696118+sl7one@users.noreply.github.com>
2022-10-19 17:42:21 +0300

    update task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+buttonEl.addEventListener('click', onClick);
+
+function onClick() {
+  const spanEl = document.querySelector('.color');
+  spanEl.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+}
