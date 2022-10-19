9cd1d304baaed19064c77228775452c420054e4d
Slava <110696118+sl7one@users.noreply.github.com>
2022-10-19 17:28:25 +0300

    udate task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const emailInputEl = document.querySelector('[name="email"]');
+const passwordInputEl = document.querySelector('[name="password"]');
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmitChange);
+
+function onSubmitChange(event) {
+  event.preventDefault();
+
+  const data = {
+    email: event.target.elements.email.value,
+    password: event.target.elements.password.value,
+  };
+
+  if (!data.email || !data.password) {
+    return alert('Не все поля заполнены');
+  }
+
+  console.log(data);
+
+  formEl.reset();
+}
+
+//
